d66d99d0f021c24432ed2b7e89dfe54fb9bfb531
sarvjeetkushwaha2002 <sarvjeetkushwaha2002@gmail.com>
2023-01-15 18:45:12 +0530

    admin changes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,18 @@
 <?php
 
 use App\Http\Controllers\Home\HomeController;
+use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\Admin\AqarController;
+use App\Http\Controllers\Admin\Auth\LoginController;
+use App\Http\Controllers\Admin\CriteriaController;
+use App\Http\Controllers\Admin\EventController;
+use App\Http\Controllers\Admin\GalleryController;
+use App\Http\Controllers\Admin\NoticeController;
+use App\Http\Controllers\Admin\PermissionController;
+use App\Http\Controllers\Admin\RoleController;
+use App\Http\Controllers\Admin\UserController;
+use App\Http\Controllers\Admin\BannerController;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -25,3 +37,40 @@ Route::get('/pro-vice-chancellor', [HomeController::class, 'proViceChancellor'])
 Route::get('/registrar', [HomeController::class, 'registrar'])->name('registrar');
 Route::get('/director', [HomeController::class, 'director'])->name('director');
 Route::get('/head', [HomeController::class, 'head'])->name('head');
+
+
+
+
+
+// Backend Routes
+Route::get('/admin',[LoginController::class, 'index'])->name('admin');
+Route::post('/login',[LoginController::class, 'store'])->name('login');
+
+Route::group(['prefix'=>'admin','as'=>'admin.', 'middleware' => 'auth'],function(){
+    Route::get('/dashboard',[AdminController::class,'dashboard'])->name('dashboard');
+    Route::get('/logout',[AdminController::class,'logout'])->name('logout');
+    Route::resource('/user', UserController::class);
+    Route::resource('/role', RoleController::class);
+    Route::resource('/permission', PermissionController::class);
+    Route::get('/role-has-permission',[PermissionController::class,'rolePermission'])->name('rolePermission');
+    Route::post('/fetch-permission',[PermissionController::class,'fetchPermission'])->name('fetchPermission');
+    Route::post('/assign-permission',[PermissionController::class,'assignPermission'])->name('assignPermission');
+    Route::resource('/notice', NoticeController::class);
+    Route::resource('/event', EventController::class);
+    Route::resource('/gallery', GalleryController::class);
+    Route::resource('/aqar', AqarController::class);
+    Route::resource('/criteria', CriteriaController::class);
+    Route::resource('/banner', BannerController::class);
+
+
+});
+
+
+Route::get('/optimize', function(){
+    Artisan::call('optimize');
+});
+Route::get('/optimize-clear', function(){
+    Artisan::call('optimize:clear');
+});
+
+
